4b1fdb5bd17bcc3816bdfbfddc54522371a27984
username** <marko.bojkic@uns.ac.rs>
2020-05-04 19:20:30 +0200

    Vezbe 8

--- FrontendRVA3/src/app/app.module.ts
+++ FrontendRVA3/src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './components/primer-components/vozilo/vozilo.component';
+import { AutomobilComponent } from './components/primer-components/automobil/automobil.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { ArtiklComponent } from './components/artikl/artikl.component';
+import { DobavljacComponent } from './components/dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './components/porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './components/stavka-porudzbine/stavka-porudzbine.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    HomeComponent,
+    AuthorComponent,
+    AboutComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
